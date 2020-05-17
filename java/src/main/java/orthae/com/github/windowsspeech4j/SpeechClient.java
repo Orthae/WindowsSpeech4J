5ef9a9082f48c9be2371e2991649c00e1befdd3d
Orthae <orthaefluorite@gmail.com>
2020-05-17 14:29:44 +0200

    speech client get/set volume/rate

--- java/src/main/java/orthae/com/github/windowsspeech4j/SpeechClient.java
+++ java/src/main/java/orthae/com/github/windowsspeech4j/SpeechClient.java
@@ -6,6 +6,7 @@ import orthae.com.github.windowsspeech4j.exception.SpeechDriverException;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.List;
 
 public class SpeechClient {
 
@@ -26,6 +27,38 @@ public class SpeechClient {
       logger.error("Failed to load driver library.");
       throw new UncheckedIOException(exception);
     }
+  }
+
+  public void setVolume(int volume) {
+    if (volume < 0) {
+      logger.warn("Volume below threshold, truncated to 0");
+      volume = 0;
+    }
+    if (volume > 100) {
+      logger.warn("Volume above threshold, truncated to 100");
+      volume = 100;
+    }
+    speechDriver.setVolume((short) volume);
+  }
+
+  public int getVolume() {
+    return speechDriver.getVolume();
+  }
+
+  public void setRate(int rate) {
+    if (rate < -10) {
+      logger.warn("Rate below threshold, truncated to -10");
+      rate = -10;
+    }
+    if (rate > 10) {
+      logger.warn("Rate above threshold, truncated to 10");
+      rate = 10;
+    }
+    speechDriver.setRate((short) rate);
+  }
+
+  public int getRate() {
+    return speechDriver.getRate();
   }
 
 
--- java/src/test/java/orthae/com/github/windowsspeech4j/SpeechClientTest.java
+++ java/src/test/java/orthae/com/github/windowsspeech4j/SpeechClientTest.java
@@ -1,6 +1,9 @@
 package orthae.com.github.windowsspeech4j;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import orthae.com.github.windowsspeech4j.exception.SpeechDriverException;
 
 import java.io.IOException;
@@ -10,6 +13,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class SpeechClientTest {
+
+  private SpeechDriverAdapter adapter;
+  private LibraryLoader loader;
+  private SpeechClient client;
+
+  @BeforeEach
+  void setup() {
+    this.adapter = mock(SpeechDriverAdapter.class);
+    this.loader = mock(LibraryLoader.class);
+    this.client = new SpeechClient(adapter, loader);
+  }
+
 
   @Test
   void constructor() throws IOException {
@@ -42,4 +57,74 @@ class SpeechClientTest {
     assertThrows(UncheckedIOException.class, () -> new SpeechClient(adapter, loader));
   }
 
+  @ParameterizedTest
+  @ValueSource(shorts = {0, 5, 10, 50, 100})
+  void setVolumeInRange(short volume) {
+    client.setVolume(volume);
+
+    verify(adapter, times(1)).setVolume(eq(volume));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-5, -10, -50, -100})
+  void setVolumeBelowRange(int volume) {
+    client.setVolume(volume);
+
+    verify(adapter, times(1)).setVolume((short) 0);
+  }
+
+  @ParameterizedTest
+  @ValueSource(shorts = {150, 200, 300, 500})
+  void setVolumeAboveRange(int volume) {
+    client.setVolume(volume);
+
+    verify(adapter, times(1)).setVolume((short) 100);
+  }
+
+  @ParameterizedTest
+  @ValueSource(shorts = {-10, -5, 0, 5, 10})
+  void setRateInRange(short rate) {
+    client.setRate(rate);
+
+    verify(adapter, times(1)).setRate(eq(rate));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-15, -30, -50, -100})
+  void setRateBelowRange(int rate) {
+    client.setRate(rate);
+
+    verify(adapter, times(1)).setRate((short) -10);
+  }
+
+  @ParameterizedTest
+  @ValueSource(shorts = {15, 30, 50, 100})
+  void setRateAboveRange(int rate) {
+    client.setRate(rate);
+
+    verify(adapter, times(1)).setRate((short) 10);
+  }
+
+
+  @ParameterizedTest
+  @ValueSource(shorts = {0, 5, 10, 50, 100})
+  void getVolume(short volume) {
+    when(adapter.getVolume()).thenReturn(volume);
+
+    final int result = client.getVolume();
+
+    verify(adapter, times(1)).getVolume();
+    assertEquals(volume, result);
+  }
+
+  @ParameterizedTest
+  @ValueSource(shorts = {-10, -5, 0, 5, 10})
+  void getRate(short rate) {
+    when(adapter.getRate()).thenReturn(rate);
+
+    final int result = client.getRate();
+
+    verify(adapter, times(1)).getRate();
+    assertEquals(rate, result);
+  }
 }
