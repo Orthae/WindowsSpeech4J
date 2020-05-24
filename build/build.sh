a08d84f1950626f0360fdd5181dc09e5c41a79e7
Orthae <orthaefluorite@gmail.com>
2020-05-24 15:35:37 +0200

    build script part 3

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,12 @@
 # 6. Failed to copy speech driver source code.
 # 7. Failed to copy speech client Maven file.
 # 8. Failed to copy speech client source code.
-
+# 9. Failed to initialize CMake.
+# 10. Failed to build speech driver.
+# 11. Failed to copy speech driver library.
+# 12. Failed to resovle Maven dependencies.
+# 13. Failed to compile speech client.
+# 14. Speech client tests failed to pass.
 
 # Terminal colors
 red=$(tput setaf 1)
@@ -52,7 +57,7 @@ fi
 if [[ -d ${buildFolder} ]] ; then
         echo "Removing old build folder" >> ${logFile}
         printf "${normal}Removing old build folder: "
-        if rm -rf ${buildFolder} &> /dev/null ; then
+        if rm -rf ${buildFolder} &>> ${logFile} ; then
                 echo "Old build folder removed" >> ${logFile}
                 printf "${green}Success\n"
         else 
@@ -66,7 +71,7 @@ fi
 echo "Creating temp build folder" >> ${logFile}
 printf "${normal}Creating build folder: "
 
-if mkdir ${buildFolder} 2> /dev/null ; then
+if mkdir ${buildFolder} &>> ${logFile} ; then
         echo "Temp folder created" >> ${logFile}
         printf "${green}Success\n"
 else
@@ -83,7 +88,7 @@ printf "${normal}Copying speech driver source code: "
 if mkdir ${buildFolder}/cpp && cp ../cpp/CMakeLists.txt ${buildFolder}/cpp/ ; then
         echo "Speech driver CMake file copied" >> ${logFile}
 else
-        echo "Copying speech driver CMake file failed" >> ${logFile}
+        echo "Build failed: Copying speech driver CMake file failed" >> ${logFile}
         printf "${red}Failure\n"
         exit 5
 fi
@@ -106,7 +111,7 @@ printf "${normal}Copying speech client source code: "
 if mkdir ${buildFolder}/java && cp ../java/pom.xml ${buildFolder}/java/ ; then
         echo "Speech client POM file copied" >> ${logFile}
 else
-        echo "Copying speech client POM file failed" >> ${logFile}
+        echo "Build failed: Copying speech client POM file failed" >> ${logFile}
         printf "${red}Failure\n"
         exit 7
 fi
@@ -116,7 +121,7 @@ if cp -r ../java/src ${buildFolder}/java/src ; then
         echo "Speech client source code copied" >> ${logFile}
         printf "${green}Success\n"
 else
-        echo "Copying client driver source code failed" >> ${logFile}
+        echo "Build failed: Copying client driver source code failed" >> ${logFile}
         printf "${red}Failure\n"
         exit 8
 fi
@@ -129,19 +134,85 @@ if cmake ${buildFolder}/cpp/CMakeLists.txt &>> ${logFile} ; then
 	echo "CMake initialized" >> ${logFile}
 	printf "${green}Success\n"
 else 
-	echo "Failed to initialize CMake" >> ${logFile}
+	echo "Build failed: Failed to initialize CMake" >> ${logFile}
 	printf "${red}Failure\n"
+	exit 9
 fi
 
 # Build speech driver
-
 echo "Building speech driver" >> ${logFile}
 printf "${normal}Building speech driver: "
+
 if cmake --build ${buildFolder}/cpp &>> ${logFile} ; then
 	echo "Speech driver builded" >> ${logFile}
 	printf "${green}Success\n"
 else
 	echo "Speech driver build failed" >> ${logFile}
 	printf "${red}Failure\n"
+	exit 10
 fi
 
+# Coping speech driver library file
+echo "Coping speech driver library file" >> ${logFile}
+printf "${normal}Coping speech driver library file: "
+
+if cp ${buildFolder}/cpp/debug/WindowsSpeech4J.dll ${buildFolder}/java/src/main/resources/ &>> ${logFile} ; then
+	echo "Speech driver library copied" >> ${logFile}
+	printf "${green}Success\n"
+else
+	echo "Build failed: Failed to copy speech driver library" >> ${logFile}
+	printf "${red}Failure\n"
+	exit 11
+fi
+
+# Resolving Maven dependencies
+echo "Resolving Maven dependencies" >> ${logFile}
+printf "${normal}Resolving Maven dependencies: "
+
+if mvn -f ${buildFolder}/java/pom.xml dependency:resolve &>> ${logFile} ; then
+	echo "Dependencies resolved" >> ${logFile}
+	printf "${green}Success\n"
+else 
+	echo "Build failed: Failed to resolve dependencies" >> ${logFile}
+	printf "${red}Failure\n"
+	exit 12
+fi
+
+# Compiling speech client
+echo "Compiling speech client" >> ${logFile}
+printf "${normal}Compiling speech client: " 
+
+if mvn -f ${buildFolder}/java/pom.xml compile &>> ${logFile} ; then
+	echo "Speech client compiled" >> ${logFile}
+	printf "${green}Success\n"	
+else 
+	echo "Build failed: Failed to compile speech client" >> ${logFile}
+	printf "${red}Failure\n"
+	exit 13
+fi
+
+# Testing speech client
+echo "Testing speech client" >> ${logFile}
+printf "${normal}Testing speech client: "
+
+if  mvn -f ${buildFolder}/java/pom.xml test &>> ${logFile} ; then
+	echo "Tests passed" >> ${logFile}
+	printf "${green}Success\n"
+else 
+	echo "Build failed: Tests failed" >> ${logFile}
+	printf "${red}Failure\n"
+	exit 14
+fi
+
+# Package project
+echo "Packing project" >> ${logFile}
+printf "Packing project: "
+
+if  mvn -f ${buildFolder}/java/pom.xml package &>> ${logFile} ; then
+	echo "Project has been packed" >> ${logFile}
+	printf "${green}Success\n"
+else 
+	echo "Failed to package project" >> ${logFile}
+	printf "${red}Failure\n"
+	exit 15
+fi
